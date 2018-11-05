51be06ae3ba10b8e8ee98467f9bd47333f984f08
佐竹涼太 <picoreamosaiko@docomo.ne.jp>
2018-11-06 07:53:07 +0900

    avatorメソッドを活性化

--- src/arachi.run.py
+++ src/arachi.run.py
@@ -87,6 +87,12 @@ async def on_message(message):
         fruit = ['りんご', 'バナナ', 'メロン','ぶどう']
         await message.channel.send(random.choice(fruit))
 
+    #アイコン確認
+    if message.content.startswith('!!avatar'):
+        embed = discord.Embed(title='Your Icon!',colour=0x2ea9ff)
+        embed.set_image(url=message.author.avater_url)
+        await message.channel.send(embed=embed)
+
     #メッセージ削除
     if message.content == ('!!delete.test'):
         await message.delete()
@@ -192,26 +198,6 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-
-    with open(YML_DATA) as stream:
-        data = yaml.load(stream)
-        greeting = data["greeting"]
-
-    loop = asyncio.get_event_loop()
-    client.loop.create_task(greeting_schedule(client.get_channel(greeting),loop))
-
-#挨拶する関数
-async def on_greeting(channel):
-    embed = discord.Embed(title='6:00になりました'
-    ,description='おはようございます。\n今日も一日がんばりましょう！',colour=0x2ea9ff)
-    await channel.send(embed=embed)
-
-#挨拶を実行する
-@asyncio.coroutine
-async def greeting_schedule(channel,loop):
-    while True:
-        if time.strftime('%H:%M:%S',time.localtime())=='6:00:00':
-            await on_greeting(channel)
 
 # 起動時
 f = open("./../data/data.yml", "r")
